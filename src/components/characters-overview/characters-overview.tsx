5876549e29e6f0ff968adf6524a8dc0ee693036a
Ahmed-Mabrook <ahmed@webbio.nl>
2020-02-14 11:09:47 +0100

    feat: created project

---
+++ src/components/characters-overview/characters-overview.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface ICharactersOverviewComponentProps {}
+
+export const CharactersOverviewComponent = () => {
+  const stay = true;
+  return (
+    <div>
+      <span>Yo nl</span>
+    </div>
+  );
+};
